a175ed23489ced355cafeab67c175393a756ef3c
Ricardo de Oliveira Goulart <ricardogoul.si@gmail.com>
2026-01-14 10:21:41 -0400

    - Projeto criado via start.spring.io - Configuração de DataSource PostgreSQL - Flyway habilitado com migration inicial

---
+++ src/main/java/com/seplag/ricardogoulart035012/Ricardogoulart035012Application.java
@@ -0,0 +1,13 @@
+package com.seplag.ricardogoulart035012;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ricardogoulart035012Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ricardogoulart035012Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/seplag/ricardogoulart035012/Ricardogoulart035012ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seplag.ricardogoulart035012;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ricardogoulart035012ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
